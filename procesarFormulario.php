77f568b921499a398124520ceba2b1f6d8f04d8f
canita402 <93312478+canita402@users.noreply.github.com>
2023-04-25 15:54:20 +0200

    Add files via upload

---
+++ procesarFormulario.php
@@ -0,0 +1,13 @@
+<?php
+$nombre = $_POST["nombre"];
+$raza = $_POST["raza"];
+$clase = $_POST["clase"];
+$nivel = $_POST["nivel"];
+
+
+
+$host= "localhost";
+$dbname="tfg_db";
+$username="root";
+$password="";
+?>
