a8636a80ee7b384a19ec7ce2a4cca1dc28ac2263
canita402 <93312478+canita402@users.noreply.github.com>
2023-05-17 15:50:43 +0200

    Delete Login directory

--- Login/MainPage.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	// Comprobar si la cookie de nombre de usuario existe y mostrar el nombre de usuario en la página
-	if (isset($_COOKIE["username"])) {
-		echo "<h1>Bienvenido " . $_COOKIE["username"] . "!</h1>";
-	} else {
-		echo "<h1>Error: no se ha iniciado sesión.</h1>";
-	}
-	?>
